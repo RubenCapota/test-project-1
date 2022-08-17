5228e1606ee31880d9f2ed7d047627cd0214431f
Ruben <r0830954@student.thomasmore.be>
2022-08-17 11:17:21 +0200

    Changes

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,5 +1,36 @@
 <template>
-  <div class="contact">
-    <h1>This is an contact page</h1>
+  <div class="about">
+    <b-form-group
+      id="fieldset-1"
+      description="Let us know your name."
+      label="Enter your name"
+      label-for="input-1"
+      valid-feedback="Thank you!"
+      :invalid-feedback="invalidFeedback"
+      :state="state"
+    >
+      <b-form-input id="input-1" v-model="name" :state="state" trim></b-form-input>
+    </b-form-group>
   </div>
 </template>
+
+<script>
+  export default {
+    computed: {
+      state() {
+        return this.name.length >= 4
+      },
+      invalidFeedback() {
+        if (this.name.length > 0) {
+          return 'Enter at least 4 characters.'
+        }
+        return 'Please enter something.'
+      }
+    },
+    data() {
+      return {
+        name: ''
+      }
+    }
+  }
+</script>
